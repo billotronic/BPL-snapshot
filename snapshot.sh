9f880ceb336072dec6c8faee9daa1092cb5a49a5
billotronic <billotronic@hush.com>
2018-03-22 12:19:10 -0500

    comments

--- snapshot.sh
+++ snapshot.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-
-ARK Blockchain Snapshot Script
-
-by tharude a.k.a The Forging Penguin
-
-11/03/2017 ARK Team
-
-Ported to Blockpool by billotronic
-
+#
+#ARK Blockchain Snapshot Script
+#
+#by tharude a.k.a The Forging Penguin
+#
+#11/03/2017 ARK Team
+#
+#Ported to Blockpool by billotronic
+#
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-
+#
 #~~~~~~~~~~~~~~~~~~~~ INSTRUCTIONS ~~~~~~~~~~~~~~~~~~~#
-
-Edit crontab by typing "sudo nano /etc/crontab"
-
-Put at the end of file the following line to make
-
-the script creating snapshots every 15 minutes:
-
-*/15 * * * * user /path/to/snapshot.sh
-
-Replace the "user" with your username
-
-and path at the end with your real script path.
-
-Save the file with Ctrl+x and Y when you're done
+#
+#Edit crontab by typing "sudo nano /etc/crontab"
+#
+#Put at the end of file the following line to make
+#
+#the script creating snapshots every 15 minutes:
+#
+#*/15 * * * * user /path/to/snapshot.sh
+#
+#Replace the "user" with your username
+#
+#and path at the end with your real script path.
+#
+#Save the file with Ctrl+x and Y when you're done
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
